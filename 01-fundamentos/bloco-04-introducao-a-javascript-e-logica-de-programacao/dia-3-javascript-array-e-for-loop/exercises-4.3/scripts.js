152e8004ac8cea13cb91a09fa247776226f5d8fb
Guarize <pedroguarize@hotmail.com>
2021-07-30 18:32:33 -0300

    Exercises 4.3 - 1 to 3

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-array-e-for-loop/exercises-4.3/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-array-e-for-loop/exercises-4.3/scripts.js
@@ -1,28 +1,13 @@
 // 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
 
-// Mostra o quadrado de *'s no HTML
+// Mostra o quadrado de *'s no console.
 
 function squareStars(num) {
   let squareSide = num;
+  let str = '';
 
-  for (let j = 1; j <= squareSide; j += 1) {
-    for (let i = 1; i <= squareSide; i += 1) {
-      document.write("*");
-    }
-    document.write('<br>')
-  }
-}
-
-console.log(squareStars(5))
-
-// Mostra o quadrado de *'s pelo console.
-
-function _squareStars(num) {
-  let squareSide = num;
-let str = '';
-
-  for (let j = 1; j <= squareSide; j += 1) {
-    for (let i = 1; i <= squareSide; i += 1) {
+  for (let column = 1; column <= squareSide; column += 1) {
+    for (let row = 1; row <= squareSide; row += 1) {
       str = str.concat('*')
     }
     str = str.concat('\n');
@@ -30,4 +15,44 @@ let str = '';
   return str;
 }
 
-console.log(_squareStars(5))
+console.log(squareStars(5));
+
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+function descendingStars(num) {
+  let hiegth = num;
+  let str = '';
+
+  for (let column = 1; column <= hiegth; column += 1) {
+    for (let row = 1; row <= column; row += 1) {
+      str = str.concat('*');
+    }
+    str = str.concat('\n')
+  }
+  return str;
+}
+
+console.log(descendingStars(5));
+
+// 3, Agora inverta o lado do triângulo. Por exemplo:
+
+function inverseDescendingStars(num) {
+  let hiegth = num + 1;
+  let str = '';
+
+  for (let column = 1; column < hiegth; column += 1) {
+    for (let row = 1; row < hiegth; row += 1) {
+      if (column + row >= hiegth) {
+        str = str.concat('*');
+      } else {
+        str = str.concat(' ');
+      }
+    }
+    str = str.concat('\n')
+  }
+  return str;
+}
+
+console.log(inverseDescendingStars(5))
+
+// Depois, faça uma pirâmide com n asteriscos de base:

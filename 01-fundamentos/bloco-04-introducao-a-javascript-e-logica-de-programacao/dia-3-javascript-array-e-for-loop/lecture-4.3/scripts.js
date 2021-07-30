72de7fbe58b35cb0d9ae6d20d4257684bb18397d
Guarize <pedroguarize@hotmail.com>
2021-07-30 17:37:10 -0300

    Diagonal attack

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-array-e-for-loop/lecture-4.3/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-array-e-for-loop/lecture-4.3/scripts.js
@@ -1,15 +1,17 @@
-let linhaRainha = 4;
+let linhaRainha = 3;
 let colunaRainha = 4;
 
-let linhaPeca = 4;
+let linhaPeca = 8;
 let colunaPeca = 7;
 
 let ataqueBemSucedido = false; 
 
-if (linhaPeca === linhaRainha) {
+if (linhaPeca === linhaRainha || colunaPeca === colunaRainha) {
   ataqueBemSucedido = true;
+} else if (Math.abs(linhaPeca - linhaRainha) == Math.abs(colunaPeca - colunaRainha)) {
+  ataqueBemSucedido = true;
+} else {
+  ataqueBemSucedido = false;
 }
 
-if (colunaPeca === colunaRainha) {
-  ataqueBemSucedido = true;
-}
+console.log(ataqueBemSucedido);

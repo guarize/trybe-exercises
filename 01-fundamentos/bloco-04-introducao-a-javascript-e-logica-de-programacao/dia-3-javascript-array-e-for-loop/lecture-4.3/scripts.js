bc1694d0fbaedda9f0953a1c6fcba18ea136c9f2
Guarize <pedroguarize@hotmail.com>
2021-07-30 16:23:52 -0300

    Horizontal and vertical attack

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-array-e-for-loop/lecture-4.3/scripts.js
@@ -0,0 +1,15 @@
+let linhaRainha = 4;
+let colunaRainha = 4;
+
+let linhaPeca = 4;
+let colunaPeca = 7;
+
+let ataqueBemSucedido = false; 
+
+if (linhaPeca === linhaRainha) {
+  ataqueBemSucedido = true;
+}
+
+if (colunaPeca === colunaRainha) {
+  ataqueBemSucedido = true;
+}

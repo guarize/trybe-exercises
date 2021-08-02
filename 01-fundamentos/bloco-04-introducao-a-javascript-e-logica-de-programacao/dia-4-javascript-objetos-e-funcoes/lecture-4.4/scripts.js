24f8bdf4e7ba5da1925c5b9eb64458b8da54a56e
Guarize <pedroguarize@hotmail.com>
2021-08-02 16:49:56 -0300

    For/in - exercises 2

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/lecture-4.4/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/lecture-4.4/scripts.js
@@ -43,3 +43,16 @@ let names = {
 for (let person in names) {
   console.log("Olá " + names[person]);
 }
+
+// 2 - Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let key in car) {
+  console.log(key, car[key])
+}
+

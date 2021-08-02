2047612313fb5d24352ba6c1f62e28668fd68496
Guarize <pedroguarize@hotmail.com>
2021-08-02 16:32:48 -0300

    Object - exercise 1 and 2

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/lecture-4.4/scripts.js
@@ -0,0 +1,18 @@
+// Objetos
+
+// 1 - Crie um objeto player contendo as variáveis listadas abaixo.
+
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3
+  },
+}
+
+// 2 - Acesse as chaves name , lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
+
+console.log("A jogadora " + player.name + " " + player.lastName + " " + "tem " + player.age + " " + "anos de idade");
+

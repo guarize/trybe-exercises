104e52649e865ad6a608d659caa64b9f9dd21edd
Guarize <pedroguarize@hotmail.com>
2021-08-02 16:54:54 -0300

    Exercises 4.4 - 1 and 2

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises-4.4/scripts.js
@@ -0,0 +1,18 @@
+// Parte I - Objetos e For/In
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+
+console.log("Bem-vinda(o), " + info.personagem)
+
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+
+info.recorrente = "Sim"
+console.log(info.recorrente)
+

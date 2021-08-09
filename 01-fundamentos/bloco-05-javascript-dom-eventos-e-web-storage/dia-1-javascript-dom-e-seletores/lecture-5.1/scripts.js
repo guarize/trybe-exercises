90aa1d1b178fa926dd2000414172080a858d8c97
Guarize <pedroguarize@hotmail.com>
2021-08-09 13:25:14 -0300

    Complete first part of lecture-5.1

---
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/lecture-5.1/scripts.js
@@ -0,0 +1,6 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById("page-title").innerText = "The Conjuring"
+document.getElementById("subtitle").innerText = "Based on the true case files of the Warrens"
+document.getElementById("second-paragraph").innerText = "Development of the film began in January 2012, and reports confirmed Wan as the director of a film entitled The Warren Files, later retitled The Conjuring, centering on the alleged real-life exploits of Ed and Lorraine Warren, a married couple who investigated paranormal events."

012c82a6dcb911b12dc2860360c6696d3887840c
Guarize <pedroguarize@hotmail.com>
2021-08-11 16:33:35 -0300

    Exercises 5.3 - Pt. I

--- 01-fundamentals/block-05-javascript-dom-events-and-web-storage/day-3-javascript-events/exercises-5.3/script.js
+++ 01-fundamentals/block-05-javascript-dom-events-and-web-storage/day-3-javascript-events/exercises-5.3/script.js
@@ -14,3 +14,31 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+// 1.
+
+function calendarDays () {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysList = document.getElementById('days');
+
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    const day = dezDaysList[i];
+    const dayItem = document.createElement('li');
+    dayItem.className = 'day';
+    dayItem.innerHTML = day;
+
+    if (day === 24 || day === 25 || day === 31) {
+      dayItem.classList.add('holiday');
+    }
+
+    if (day === 4 || day === 11 || day === 18 || day === 25) {
+      dayItem.classList.add('friday');
+    }
+
+    daysList.appendChild(dayItem);
+  }
+}
+
+calendarDays();
+
+// 2.

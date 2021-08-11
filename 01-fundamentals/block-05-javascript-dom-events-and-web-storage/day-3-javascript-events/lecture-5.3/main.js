82d00816c3020d9cfe18eefb6a721c07764b97d9
Guarize <pedroguarize@hotmail.com>
2021-08-11 15:55:30 -0300

    Completed lecture-5.3 exercises

--- 01-fundamentals/block-05-javascript-dom-events-and-web-storage/day-3-javascript-events/lecture-5.3/main.js
+++ 01-fundamentals/block-05-javascript-dom-events-and-web-storage/day-3-javascript-events/lecture-5.3/main.js
@@ -1,33 +1,54 @@
-const firstLi = document.getElementById('first-li');
-const secondLi = document.getElementById('second-li');
-const thirdLi = document.getElementById('third-li');
+const firstLi = document.getElementById('firstLi');
+const secondLi = document.getElementById('secondLi');
+const thirdLi = document.getElementById('thirdLi');
 const input = document.getElementById('input');
-const myWebpage = document.getElementById('my-spotrybefy');
-
-/*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
-
-Segue abaixo um exemplo do uso de event.target:
-*/
-
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
-
-firstLi.addEventListener('dblclick', resetText);
+const myWebpage = document.getElementById('mySpotrybefy');
 
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'firstLi'.
+
+// 1. Create a function that adds the class 'tech' to the selected element;
+// - There must be only one element with the class 'tech'. How do you do this?
+
+function addTechClass(event) {
+  const techElement = document.querySelector(".tech");
+  techElement.classList.remove('tech');
+  event.target.classList.add("tech");
+}
+
+firstLi.addEventListener("click", addTechClass);
+secondLi.addEventListener("click", addTechClass);
+thirdLi.addEventListener("click", addTechClass);
+
+// Create a function that, when typed into the text box, changes the text of the element
+// element with the class 'tech;
+
+function changeText() {
+  document.querySelector('.tech').innerText = input.value;
+}
+
+input.addEventListener("keyup", changeText);
+
+// Create a function that, when double-clicking 'Meu top 3 do Spotrybefy', it
+// redirects to some page;
+// 1. How about redirecting to your portfolio?
+
+function redirectToGithub(event) {
+  window.open("https://github.com/guarize")
+}
+
+myWebpage.addEventListener("dblclick", redirectToGithub)
+
+// Create a function that, when you hover the mouse over 'My Spotrybefy top 3', changes
+// its color;
+
+function hoverColor(event) {
+  event.target.style.color = 'yellow';
+}
+
+myWebpage.addEventListener("mouseover", hoverColor);
+
+myWebpage.addEventListener("mouseleave", function(event) {
+  event.target.style.color = "white"
+});

886d7f5feac12eb8dd40eb7909d23384b288e89a
Guarize <pedroguarize@hotmail.com>
2021-08-18 21:38:17 -0300

    Added states dinamically

---
+++ 01-fundamentals/block-06-html-css-forms-flexbox-and-responsive/day-1-html-css-forms/exercises-6.1/script.js
@@ -0,0 +1,17 @@
+const dropState = document.getElementById('state');
+
+const arraySiglaEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RO', 'RS', 'RR', 'SC', 'SE', 'SP', 'TO'];
+
+const arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+
+function addStates() {
+  for (let i = 0; i < arrayTextoEstado.length; i += 1) {
+    let newState = document.createElement('option');
+    newState.id = arraySiglaEstado[i];
+    newState.innerHTML = arrayTextoEstado[i];
+    console.log(newState);
+    dropState.appendChild(newState);
+  }
+}
+
+addStates();

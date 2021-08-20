430632f3b06cbe25e5d2bd88bc51980f6e1de7ca
Guarize <pedroguarize@hotmail.com>
2021-08-19 21:38:13 -0300

    Final push

--- 01-fundamentals/block-06-html-css-forms-flexbox-and-responsive/day-2-javascript-libraries-and-css-frameworks/exercises-6.2/script.js
+++ 01-fundamentals/block-06-html-css-forms-flexbox-and-responsive/day-2-javascript-libraries-and-css-frameworks/exercises-6.2/script.js
@@ -1,5 +1,31 @@
-import './node_modules/just-validate/dist/js/just-validate.js'
+import "./node_modules/just-validate/dist/js/just-validate.js";
 
 window.onload = function () {
-  new window.JustValidate('.js-form');
-}
+  new window.JustValidate(".js-form", {
+    rules: {
+      date: {
+        required: true,
+      },
+    },
+  });
+};
+
+const picker = new Pikaday({ field: $('#datepicker')[0],
+  format: 'D/M/YYYY',
+  toString(date, format) {
+    // you should do formatting based on the passed format,
+    // but we will just return 'D/M/YYYY' for simplicity
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+    // dateString is the result of `toString` method
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  }
+});

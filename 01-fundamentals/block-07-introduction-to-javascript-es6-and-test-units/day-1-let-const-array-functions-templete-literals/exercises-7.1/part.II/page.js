ce1cfd255cb1019aaac3d6900d2255a8255b06b6
Guarize <pedroguarize@hotmail.com>
2021-08-27 17:04:33 -0300

    Completed exercises-7.1 - part.II - 3

---
+++ 01-fundamentals/block-07-introduction-to-javascript-es6-and-test-units/day-1-let-const-array-functions-templete-literals/exercises-7.1/part.II/page.js
@@ -0,0 +1,24 @@
+const btn = document.getElementById('btn');
+const stringP = document.getElementById('string');
+const nameInput = document.getElementById('name');
+const skillsBtn = document.getElementById('skills-btn');
+const skillsP = document.getElementById('skills-p')
+const str = 'Tryber x aqui';
+const skills = ['JavaScript', 'CSS', 'HTML', 'Git', 'Bootstrap']
+
+let clickCount = 0;
+const counter = document.getElementById('counter');
+counter.innerText = clickCount;
+
+btn.addEventListener('click', () => {
+  counter.innerText = Number(counter.innerText) + 1;
+});
+
+nameInput.addEventListener('blur', () => {
+  stringP.innerText = str.replace(/x/g, nameInput.value);
+});
+
+skillsBtn.addEventListener('click', () => {
+  const strSkills = skills.toString().replace(/,/g, '; ');
+  skillsP.innerText = `${stringP.innerText}! Minhas cinco principais habilidades são: ${strSkills} #goTrybe`;
+});

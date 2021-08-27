f5106386f5e89d9648754207e26b4a590ac3def3
Guarize <pedroguarize@hotmail.com>
2021-08-27 16:34:29 -0300

    Completed exercise-7.1 - part.II - 1 & 2

---
+++ 01-fundamentals/block-07-introduction-to-javascript-es6-and-test-units/day-1-let-const-array-functions-templete-literals/exercises-7.1/part.II/script.js
@@ -0,0 +1,34 @@
+// 1 - Create a function that takes a number and returns its factorial.
+
+const factorial = (number) => {
+  if (number === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 1; i <= number; i += 1) {
+      result *= i;
+    }
+    return result;
+  }
+};
+
+// Bonus (optional): try doing the same exercise recursively.
+
+const factorialRec = (number) => number === 0 ? 1 : number * factorial(number - 1);
+
+// 2 - Create a function that takes a sentence and returns the largest word.
+
+const largestWord = (str) => {
+  let strSplit = str.split(' ');
+  let result = '';
+  for (let i = 0; i < strSplit.length; i += 1) {
+    if (strSplit[i].length > result.length) {
+      result = strSplit[i];
+    }
+  }
+  return result;
+}
+
+// Refactoring
+
+const largestWord2 = (str) => str.split(' ').sort((a, b) => a.length - b.length).pop();

56444079db56ca8457a4eb0f54ad2e71a4b28a43
Guarize <pedroguarize@hotmail.com>
2021-08-31 15:42:49 -0300

    Exercise 2 - myRemove()

---
+++ 01-fundamentals/block-07-introduction-to-javascript-es6-and-test-units/day-3-unit-tests/exercises-7.3/myRemove.js
@@ -0,0 +1,26 @@
+// The function myRemove(arr, item) takes an array arr and returns a copy of that array without the 'item' element if it exists in the array.
+// [x] Check that the call myRemove([1, 2, 3, 4], 3) returns the expected array.
+// [x] Check that the call myRemove([1, 2, 3, 4], 3) does not return the array [1, 2, 3, 4].
+// [x] Check that the array passed as parameter has not changed.
+// [] Check that the call myRemove([1, 2, 3, 4], 5) returns the expected array
+
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4])
+
+const testArray = [1, 2, 3, 4, 5];
+myRemove(testArray, 2);
+assert.deepStrictEqual(testArray, [1, 2, 3, 4, 5])
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4])
--- 01-fundamentals/block-07-introduction-to-javascript-es6-and-test-units/day-3-unit-tests/exercises-7.3/sum.js
+++ 01-fundamentals/block-07-introduction-to-javascript-es6-and-test-units/day-3-unit-tests/exercises-7.3/sum.js
@@ -14,7 +14,11 @@ function sum(a, b) {
   return a + b;
 }
 
-sum(4, '5')
-
 assert.strictEqual(sum(4, 5), 9, '4 + 5 = 9');
 assert.strictEqual(sum(0, 0), 0, '0 + 0 = 0');
+assert.throws(() => {
+  sum(4, '5')
+});
+assert.throws(() => {
+  sum(4, '5')
+}, /^Error: parameters must be numbers$/)

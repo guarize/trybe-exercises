8dcf5ee74b3d5c82f35da989ab2b99f2be8933fe
Guarize <pedroguarize@hotmail.com>
2021-09-10 15:52:24 -0300

    Exercise - 4

---
+++ 01-fundamentals/block-08-high-order-functions-javascript-es6/day-5-js-es6-spread-operator-rest-parameter-destructuring-+/exercises-8.5/exercise4.js
@@ -0,0 +1,42 @@
+// 4 - Write a function filterPeople that, given a list of people, returns all Australian people who were born in the 20th century:
+
+const assert = require('assert');
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filterPeople = (arr) => arr.filter(({ bornIn, nationality}) => 
+bornIn > 1900 && bornIn <= 2000 && nationality === 'Australian'
+);
+
+const filteredPeople = filterPeople(people);
+
+console.log(filteredPeople);
+
+assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
+assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });

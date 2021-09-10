7f420186efd0e2e2032b6dd15ffdb6d9c97b77d4
Guarize <pedroguarize@hotmail.com>
2021-09-10 15:32:14 -0300

    Exercise - 1

---
+++ 01-fundamentals/block-08-high-order-functions-javascript-es6/day-5-js-es6-spread-operator-rest-parameter-destructuring-+/exercises-8.5/exercicio1.js
@@ -0,0 +1,14 @@
+// 1 - Given the code below, complete it so that it prints the area of the 3 rectangles. The code should return in sequence 2 , 15 and 54.
+
+const assert = require('assert');
+
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rectangle) => {
+  assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]);
+});

47d3540416293b0a4e7fe41a8e4ccc5ba8f083e5
Guarize <pedroguarize@hotmail.com>
2021-09-10 15:58:25 -0300

    Exercises - 5 & 6

---
+++ 01-fundamentals/block-08-high-order-functions-javascript-es6/day-5-js-es6-spread-operator-rest-parameter-destructuring-+/exercises-8.5/exercise5.js
@@ -0,0 +1,17 @@
+// 5 - Write a swap function, which given an array of 3 elements, returns a new array with the first and third elements swapped. Detail: you need to do this function spending only 1 line:
+
+const assert = require('assert');
+
+const myList = [1, 2, 3];
+
+// const swap = (arr) => arr.reverse(); 
+
+const swap = ([a, b, c]) => [c, b, a];
+
+const swappedList = swap(myList);
+
+console.log(swappedList);
+
+assert.strictEqual(swappedList[0], 3);
+assert.strictEqual(swappedList[1], 2);
+assert.strictEqual(swappedList[2], 1);

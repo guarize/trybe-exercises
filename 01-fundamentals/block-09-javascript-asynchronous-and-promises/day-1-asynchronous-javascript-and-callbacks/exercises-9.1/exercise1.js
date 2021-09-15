8c5f1231ba14b17ab3cb5dbdedbfe53861d763f0
Guarize <pedroguarize@hotmail.com>
2021-09-15 16:07:28 -0300

    Exercise - 1

---
+++ 01-fundamentals/block-09-javascript-asynchronous-and-promises/day-1-asynchronous-javascript-and-callbacks/exercises-9.1/exercise1.js
@@ -0,0 +1,14 @@
+// 1 - The function getPlanet below prints the planet Mars synchronously. Modify getPlanet so that Mars is printed asynchronously, after 4 seconds.
+
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  setTimeout(() => console.log("Returned planet: ", mars), 4000)
+};
+
+getPlanet();

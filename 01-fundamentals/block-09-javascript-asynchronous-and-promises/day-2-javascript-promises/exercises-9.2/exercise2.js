304d4c7f4ea5401531fce3bdce29545d887c5893
Guarize <pedroguarize@hotmail.com>
2021-09-16 16:28:36 -0300

    Exercise - 3 & 4

--- 01-fundamentals/block-09-javascript-asynchronous-and-promises/day-2-javascript-promises/exercises-9.2/exercise2.js
+++ 01-fundamentals/block-09-javascript-asynchronous-and-promises/day-2-javascript-promises/exercises-9.2/exercise2.js
@@ -1,14 +1,14 @@
 const promise = new Promise((resolve, reject) => {
-  const arr = Array.from({ length: 10 }, () => Math.floor(Math.random() * 50) + 1)
+  const arr = Array.from({ length: 10 }, () => Math.floor(Math.random() * 50) + 1);
   const result = arr.map((number) => number * number)
   .reduce((acc, curr) => acc + curr, 0);
   
   if (result < 8000) {
-    resolve(`Promise was resolved, number: ${result}`)
+    resolve(result);
   }
-  reject(`Fails! Number: ${result}`)
+  reject('É mais de oito mil! Essa promise deve estar quebrada!')
 })
-.then(msg => console.log(msg))
+.then(sum => console.log([2, 3, 5, 10].map((number) => sum / number)))
 .catch(error => console.log(error))
 
 promise;

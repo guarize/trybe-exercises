bd6cc65a0ca582bdfc55e792020e67c338ad2267
Guarize <pedroguarize@hotmail.com>
2021-10-05 12:54:58 -0300

    Exercise 1

--- 02-front-end/bloco-11-introducao-a-react/day-1-hello-world-react/exercises-app/src/App.js
+++ 02-front-end/bloco-11-introducao-a-react/day-1-hello-world-react/exercises-app/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const activities = ['Homework', 'Dishes', 'Groceries', 'Project']
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>{ activities.map(activitie => Task(activitie))}</ul>
       </header>
     </div>
   );

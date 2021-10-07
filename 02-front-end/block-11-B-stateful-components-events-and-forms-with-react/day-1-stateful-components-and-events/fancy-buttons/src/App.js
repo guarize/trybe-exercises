d906174c5991ce655c6961c9f00f83a498b572bc
Guarize <pedroguarize@hotmail.com>
2021-10-07 18:57:33 -0300

    Fancy Buttons clickHandler

--- 02-front-end/block-11-B-stateful-components-events-and-forms-with-react/day-1-stateful-components-and-events/fancy-buttons/src/App.js
+++ 02-front-end/block-11-B-stateful-components-events-and-forms-with-react/day-1-stateful-components-and-events/fancy-buttons/src/App.js
@@ -1,28 +1,56 @@
 import React from 'react';
 import './App.css';
 
-
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstBtnClicks: 0,
+      secondBtnClicks: 0,
+      thirdBtnClicks: 0,
+    };
+  }
 
   handleFirstBtn = () => {
-    console.log("First!");
-  }
-  
+    this.setState((previousState, _props) => ({
+      firstBtnClicks: previousState.firstBtnClicks + 1,
+    }));
+  };
+
   handleSecondBtn = () => {
-    console.log("Second!");
-  }
-  
+    this.setState((previousState, _props) => ({
+      secondBtnClicks: previousState.secondBtnClicks + 1,
+    }));
+  };
+
   handleThirdBtn = () => {
-    console.log("Third!");
-  }
+    this.setState((previousState, _props) => ({
+      thirdBtnClicks: previousState.thirdBtnClicks + 1,
+    }));
+  };
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <div>
-          <button onClick={this.handleFirstBtn} className="Buttons first-button">First</button>
-          <button onClick={this.handleSecondBtn} className="Buttons second-button">Second</button>
-          <button onClick={this.handleThirdBtn} className="Buttons third-button">Third</button>
+          <button
+            onClick={this.handleFirstBtn}
+            className='Buttons first-button'
+          >
+            {this.state.firstBtnClicks}
+          </button>
+          <button
+            onClick={this.handleSecondBtn}
+            className='Buttons second-button'
+          >
+            {this.state.secondBtnClicks}
+          </button>
+          <button
+            onClick={this.handleThirdBtn}
+            className='Buttons third-button'
+          >
+            {this.state.thirdBtnClicks}
+          </button>
         </div>
       </div>
     );

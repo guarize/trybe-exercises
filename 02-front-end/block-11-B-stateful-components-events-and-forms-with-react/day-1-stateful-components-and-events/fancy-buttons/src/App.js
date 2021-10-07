b077ef3f576b6e21953f513c80af4eed0c0f04bb
Guarize <pedroguarize@hotmail.com>
2021-10-07 18:40:54 -0300

    Fancy Buttons events

--- 02-front-end/block-11-B-stateful-components-events-and-forms-with-react/day-1-stateful-components-and-events/fancy-buttons/src/App.js
+++ 02-front-end/block-11-B-stateful-components-events-and-forms-with-react/day-1-stateful-components-and-events/fancy-buttons/src/App.js
@@ -1,11 +1,32 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends React.Component {
+
+  handleFirstBtn = () => {
+    console.log("First!");
+  }
+  
+  handleSecondBtn = () => {
+    console.log("Second!");
+  }
+  
+  handleThirdBtn = () => {
+    console.log("Third!");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <button onClick={this.handleFirstBtn} className="Buttons first-button">First</button>
+          <button onClick={this.handleSecondBtn} className="Buttons second-button">Second</button>
+          <button onClick={this.handleThirdBtn} className="Buttons third-button">Third</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

d3c8e397d4f1b16e575c0a30c551054c051ba2bc
Guarize <pedroguarize@hotmail.com>
2021-10-07 20:53:49 -0300

    Refactor pokedex exercise 1

--- 02-front-end/block-11-B-stateful-components-events-and-forms-with-react/day-1-stateful-components-and-events/my-pokedex/src/components/Pokedex.jsx
+++ 02-front-end/block-11-B-stateful-components-events-and-forms-with-react/day-1-stateful-components-and-events/my-pokedex/src/components/Pokedex.jsx
@@ -14,14 +14,17 @@ class Pokedex extends React.Component {
     this.setState((previous, props) => {
       const { pokemons } = props;
 
-      if (previous.pokemonDisplay === pokemons.length - 1) {
-        return ({ pokemonDisplay: 0})
-      }
-
-      return ({
-        pokemonDisplay: previous.pokemonDisplay + 1,
-      });
+      return {
+        pokemonDisplay:
+          previous.pokemonDisplay === pokemons.length - 1
+            ? 0
+            : previous.pokemonDisplay + 1,
+      };
     });
+  };
+
+  fire = () => {
+    return;
   };
 
   render() {
@@ -31,11 +34,20 @@ class Pokedex extends React.Component {
 
     return (
       <div className='pokedex-container'>
+        <div className='elements-container'>
+          <button className='fire'>Fire</button>
+          <button className='psychic'>Psychic</button>
+        </div>
         <button onClick={this.nextPokemon} className='next-btn'>
           Next
         </button>
         <div className='pokedex'>
-          { <Pokemon key={pokemons[pokemonDisplay].id} pokemon={pokemons[pokemonDisplay]} /> }
+          {
+            <Pokemon
+              key={pokemons[pokemonDisplay].id}
+              pokemon={pokemons[pokemonDisplay]}
+            />
+          }
         </div>
       </div>
     );

e6326830390883c971aae3156157dfa6833ad651
Guarize <pedroguarize@hotmail.com>
2021-10-18 12:02:28 -0300

    Imports original src

--- 02-front-end/block-12-life-cycle-components-and-react-router/day-2-react-router/exercise-pokedex-router-master/src/App.js
+++ 02-front-end/block-12-life-cycle-components-and-react-router/day-2-react-router/exercise-pokedex-router-master/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
+import Pokedex from './components/Pokedex.jsx';
 import pokemons from './data';
-import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Pokedex</h1>
+        <div className='pokedex'>
+          <Pokedex pokemons={ pokemons } />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

81f29da89d976fafcca269f2700d1b0a82421a49
Guarize <pedroguarize@hotmail.com>
2021-11-04 18:35:19 -0300

    Exercise - 2 & 3

--- 02-front-end/block-14-automated-testing-with-react-testing-library/day-3-rtl-testing-react-router/exercises-14.3/src/App.js
+++ 02-front-end/block-14-automated-testing-with-react-testing-library/day-3-rtl-testing-react-router/exercises-14.3/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 export const About = () => <h1>Você está na página Sobre</h1>;
 export const Home = () => <h1>Você está na página Início</h1>;
@@ -11,11 +11,11 @@ export default function App() {
       <Link to="/">Início</Link>
       <br />
       <Link to="/about">Sobre</Link>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route element={<NoMatch />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
   );
 }
--- 02-front-end/block-14-automated-testing-with-react-testing-library/day-3-rtl-testing-react-router/exercises-14.3/src/renderWithRouter.js
+++ 02-front-end/block-14-automated-testing-with-react-testing-library/day-3-rtl-testing-react-router/exercises-14.3/src/renderWithRouter.js
@@ -10,5 +10,4 @@ const renderWithRouter = (component) => {
     history,
   };
 };
-
 export default renderWithRouter;

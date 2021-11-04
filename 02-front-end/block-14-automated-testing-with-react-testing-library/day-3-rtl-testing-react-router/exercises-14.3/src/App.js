6eb7a1a6f598890aa26d4cc9d37ba558ed094000
Guarize <pedroguarize@hotmail.com>
2021-11-04 18:24:43 -0300

    Exercise - 1

--- 02-front-end/block-14-automated-testing-with-react-testing-library/day-3-rtl-testing-react-router/exercises-14.3/src/App.js
+++ 02-front-end/block-14-automated-testing-with-react-testing-library/day-3-rtl-testing-react-router/exercises-14.3/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+export const About = () => <h1>Você está na página Sobre</h1>;
+export const Home = () => <h1>Você está na página Início</h1>;
+export const NoMatch = () => <h1>Página não encontrada</h1>;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link to="/">Início</Link>
+      <br />
+      <Link to="/about">Sobre</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<NoMatch />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;

c739eeb599bda1f1caf2f506c24254a9cbde57db
Guarize <pedroguarize@hotmail.com>
2021-11-18 15:43:59 -0300

    Very basic CSS

--- 02-front-end/block-15-state-management-with-redux/day-5-react-redux-tests/counter-redux/src/App.test.js
+++ 02-front-end/block-15-state-management-with-redux/day-5-react-redux-tests/counter-redux/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 import { createStore, combineReducers } from 'redux';
 import clickReducer from './reducers';
@@ -20,11 +21,28 @@ const renderWithRedux = (
 
 describe('testing clicks', () => {
   beforeEach(cleanup);
+
   test('the page should have a button and a text 0', () => {
-    const { queryByText } = renderWithRedux(<App />);
+    const { queryByText } = renderWithRedux(<App />, {
+      initialState: { clickReducer: { counter: 5 } },
+    });
     const buttonAdicionar = queryByText('Add');
 
     expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('5')).toBeInTheDocument();
+  });
+
+  test('the button should work', () => {
+    const { queryByText } = renderWithRedux(<App />, {
+      initialState: { clickReducer: { counter: 0 } },
+    });
+
+    const buttonAdicionar = queryByText('Add');
+    expect(buttonAdicionar).toBeInTheDocument();
     expect(queryByText('0')).toBeInTheDocument();
+
+    userEvent.click(buttonAdicionar);
+
+    expect(queryByText('1')).toBeInTheDocument();
   });
 });
--- 02-front-end/block-15-state-management-with-redux/day-5-react-redux-tests/counter-redux/src/NumberClicks.jsx
+++ 02-front-end/block-15-state-management-with-redux/day-5-react-redux-tests/counter-redux/src/NumberClicks.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 class NumberClicks extends Component {
   render() {
-    return <div>{this.props.counter}</div>;
+    return <div className="counter">{this.props.counter}</div>;
   }
 }
 

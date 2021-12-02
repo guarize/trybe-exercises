6519a37be6c0331c714ef7199becfa38eb9bfaee
Guarize <pedroguarize@hotmail.com>
2021-12-01 23:22:56 -0300

    Basic CSS

--- 02-front-end/block-16-context-api-and-react-hooks/day-1-react-context-api/context-api/src/App.jsx
+++ 02-front-end/block-16-context-api-and-react-hooks/day-1-react-context-api/context-api/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 import MyContext from './contexts/MyContext';
+import './App.css'
 
 class App extends Component {
   constructor() {

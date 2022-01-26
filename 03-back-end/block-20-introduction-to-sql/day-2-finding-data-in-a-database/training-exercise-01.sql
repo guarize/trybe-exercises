0c5faaad0a6c1ee93764d6280bbfb99e5d59b7f8
Guarize <pedroguarize@hotmail.com>
2022-01-26 19:14:16 -0300

    [feat] - 1~4 training exercises

---
+++ 03-back-end/block-20-introduction-to-sql/day-2-finding-data-in-a-database/training-exercise-01.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sakila.city;
+
+SELECT first_name, last_name FROM sakila.customer;
+
+SELECT * FROM sakila.rental;
+
+SELECT title, description, release_year FROM sakila.film;
---
+++ 03-back-end/block-20-introduction-to-sql/day-2-finding-data-in-a-database/training-exercise-03.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT nome, idade FROM Escola.Alunos;
+
+SELECT DISTINCT nome FROM Escola.Alunos;
+
+SELECT DISTINCT idade FROM Escola.Alunos;

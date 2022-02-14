2cfa1b3d4c44a80b759d9da6290ead52dcfb39a4
Guarize <pedroguarize@hotmail.com>
2022-02-13 21:06:36 -0300

    [test] - passing grade function testing

--- 03-back-end/block-22-introduction-to-web-development-with-node.js/day-3-mocha-chai-sinon-backend-testing-with-node/passing-grade/index.js
+++ 03-back-end/block-22-introduction-to-web-development-with-node.js/day-3-mocha-chai-sinon-backend-testing-with-node/passing-grade/index.js
@@ -1,28 +1,3 @@
 const passingGrade = require('./passingGrade');
 
-console.log('Quando a média for menor que 7, retorna "Fail":');
-
-const respostaCenario1 = passingGrade(4);
-if (respostaCenario1 === 'Fail') {
-  console.log(`Ok 🚀`);
-} else {
-  console.error('Resposta não esperada 🚨');
-}
-
-console.log('Quando a média for maior que 7, retorna "Pass":');
-
-const respostaCenario2 = passingGrade(9);
-if (respostaCenario2 === 'Pass') {
-  console.log(`Ok 🚀`);
-} else {
-  console.error('Resposta não esperada 🚨');
-}
-
-console.log('Quando a média for igual a 7, retorna "Pass":');
-
-const respostaCenario3 = passingGrade(7);
-if (respostaCenario3 === 'Pass') {
-  console.log(`Ok 🚀`);
-} else {
-  console.error('Resposta não esperada 🚨');
-}
+console.log(passingGrade(7));
---
+++ 03-back-end/block-22-introduction-to-web-development-with-node.js/day-3-mocha-chai-sinon-backend-testing-with-node/passing-grade/tests.js
@@ -0,0 +1,16 @@
+const passingGrade = require('./passingGrade');
+const { expect } = require('chai');
+
+describe('Quando a média for menor que 7', () => {
+  it('retorna "Fail"', () => {
+    const response = passingGrade(4);
+    expect(response).to.be.equals('Fail');
+  });
+});
+
+describe('Quando a média for maior que 7', () => {
+  it('retorna "Pass"', () => {
+    const response = passingGrade(7);
+    expect(response).to.be.equals('Pass');
+  });
+});

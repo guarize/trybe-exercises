93e4d9b8dee10ad6275b26103aceebf838d2b5b3
Guarize <pedroguarize@hotmail.com>
2022-02-13 21:14:53 -0300

    [test] - passing grade function more assertions

--- 03-back-end/block-22-introduction-to-web-development-with-node.js/day-3-mocha-chai-sinon-backend-testing-with-node/passing-grade/tests.js
+++ 03-back-end/block-22-introduction-to-web-development-with-node.js/day-3-mocha-chai-sinon-backend-testing-with-node/passing-grade/tests.js
@@ -10,7 +10,15 @@ describe('Quando a média for menor que 7', () => {
 
 describe('Quando a média for maior que 7', () => {
   it('retorna "Pass"', () => {
+    const response = passingGrade(9);
+    expect(response).to.be.equals('Pass');
+  });
+});
+
+describe('Quando a média for igual a 7', () => {
+  it('retorna "Pass"', () => {
     const response = passingGrade(7);
     expect(response).to.be.equals('Pass');
+    expect(response).not.equals('Fail');
   });
 });

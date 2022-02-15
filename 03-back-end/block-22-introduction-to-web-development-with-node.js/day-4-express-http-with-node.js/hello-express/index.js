6e84aeadcbc7607812462a77290f9413c916c820
Guarize <pedroguarize@hotmail.com>
2022-02-14 21:50:21 -0300

    [feat] - recipes app api

--- 03-back-end/block-22-introduction-to-web-development-with-node.js/day-4-express-http-with-node.js/hello-express/index.js
+++ 03-back-end/block-22-introduction-to-web-development-with-node.js/day-4-express-http-with-node.js/hello-express/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/hello', handleHelloWorldRequest);
+app.get('/hello', (req, res) => {
+  res.status(200).send('Hello World!');
+});
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta http://localhost:3001/hello');
 });
-
-function handleHelloWorldRequest(req, res) {
-  res.status(200).send('Hello World!');
-}

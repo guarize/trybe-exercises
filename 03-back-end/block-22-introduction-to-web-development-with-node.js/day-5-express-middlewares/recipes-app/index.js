024ed78c6b70866fd30e96d115fb7de9fbc81688
Guarize <pedroguarize@hotmail.com>
2022-02-15 09:48:31 -0300

    [feat] - middleware error handling

--- 03-back-end/block-22-introduction-to-web-development-with-node.js/day-5-express-middlewares/recipes-app/index.js
+++ 03-back-end/block-22-introduction-to-web-development-with-node.js/day-5-express-middlewares/recipes-app/index.js
@@ -4,75 +4,18 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const recipes = [
-  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
-  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
-  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
-];
-
-const validatePrice = (req, res, next) => {
-  const { price } = req.body;
-  if (price <= 0 || typeof price !== 'number')
-    res.status(404).json({ message: 'Price not valid!' });
-
-  next();
-};
-
-app.get('/recipes', function (req, res) {
-  res.status(200).json(recipes);
+app.get('/open', function (req, res) {
+  res.send('open!');
 });
 
-app.get('/recipes/search', function (req, res) {
-  const { name, maxPrice } = req.query;
-  const filteredRecipes = recipes.filter(
-    (r) => r.name.includes(name) && r.price < parseInt(maxPrice),
-  );
-  res.status(200).json(filteredRecipes);
-});
+const recipesRouter = require('./routes/recipesRouter');
 
-app.get('/recipes/:id', function (req, res) {
-  const { id } = req.params;
-  const recipe = recipes.find((r) => r.id === parseInt(id));
-  if (!recipe) return res.status(404).json({ message: 'Recipe not found!' });
-
-  res.status(200).json(recipe);
-});
-
-app.post('/recipes', validatePrice, function (req, res) {
-  const { id, name, price, waitTime } = req.body;
-  recipes.push({ id, name, price, waitTime });
-  res.status(201).json({ message: 'Recipe created successfully!' });
-});
-
-app.put('/recipes/:id', function (req, res) {
-  const { id } = req.params;
-  const { name, price, waitTime } = req.body;
-  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
-
-  if (recipeIndex === -1)
-    return res.status(404).json({ message: 'Recipe not found!' });
-
-  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price, waitTime };
-
-  res.status(204).end();
-});
-
-app.delete('/recipes/:id', function (req, res) {
-  const { id } = req.params;
-  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
-
-  if (recipeIndex === -1)
-    return res.status(404).json({ message: 'Recipe not found!' });
-
-  recipes.splice(recipeIndex, 1);
-
-  res.status(204).end();
-});
+app.use('/recipes', recipesRouter);
 
 app.all('*', function (req, res) {
   return res.status(404).json({ message: `Rota '${req.path}' não existe!` });
 });
 
 app.listen(3001, () => {
-  console.log('Aplicação ouvindo na porta 3001');
+  console.log('Ouvindo na porta http://localhost:3001');
 });

e592e4c681e9d7f7d139a1f56e199476f236d323
Guarize <pedroguarize@hotmail.com>
2022-02-21 11:59:50 -0300

    feat: authors models + services + controllers

---
+++ 03-back-end/block-23-node.js-service-layer-rest-and-restful-architecture/day-2-software-architecture-controller-and-service-layer/authors/services/Authors.js
@@ -0,0 +1,71 @@
+const Author = require('../models/Authors');
+
+const getNewAuthor = (authorData) => {
+  const { id, firstName, middleName, lastName } = authorData;
+
+  const fullName = [firstName, middleName, lastName]
+    .filter((name) => name)
+    .join(' ');
+
+  return {
+    id,
+    firstName,
+    middleName,
+    lastName,
+    name: fullName,
+  };
+};
+
+const isValid = (firstName, middleName, lastName) => {
+  if (!firstName || typeof firstName !== 'string') return false;
+  if (!lastName || typeof lastName !== 'string') return false;
+  if (middleName && typeof middleName !== 'string') return false;
+
+  return true;
+};
+
+const getAll = async () => {
+  const authors = await Author.getAll();
+
+  return authors.map(getNewAuthor);
+};
+
+const findById = async (id) => {
+  const author = await Author.findById(id);
+
+  if (!author) {
+    return {
+      error: {
+        code: 'notFound',
+        message: `Não foi possível encontrar uma pessoa autora com o id ${id}`,
+      },
+    };
+  }
+
+  return author;
+};
+
+const createAuthor = async (firstName, middleName, lastName) => {
+  const existingAuthor = await Author.findByName(
+    firstName,
+    middleName,
+    lastName,
+  );
+
+  if (existingAuthor) {
+    return {
+      error: {
+        code: 'alreadyExists',
+        message: 'Uma pessoa autora já existe com esse nome completo',
+      },
+    };
+  }
+
+  return Author.createAuthor(firstName, middleName, lastName);
+};
+
+module.exports = {
+  getAll,
+  findById,
+  createAuthor,
+};
